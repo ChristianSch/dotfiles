f1b791d305634ffae79377a64221421f28dced6c
Christian Schulze <accounts@andinfinity.de>
2017-09-27 10:27:37 +0200

    polybar is now displayed on all monitors

--- arch/polybar/launch.sh
+++ arch/polybar/launch.sh
@@ -6,7 +6,20 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# starts a polybar instance on all monitors
+
+IFS='
+'
+set -o noglob
+
+displays=($(xrandr | grep " connected" | awk -F'[ +]' '{print $1}'))
+
+for display in "${displays[@]}"
+do
+    echo "starting polybar 'main_bar' on env MONITOR="$display
+    env MONITOR=$display polybar main_bar &
+done
+
 # Launch bar1 and bar2
-polybar main_bar &
 
 echo "Bars launched..."
